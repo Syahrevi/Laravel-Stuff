83af6ac492f72949d8bdb86074ce1444b56ff72d
Muhammad Syahrevi <55977012+lizardhazard@users.noreply.github.com>
2020-07-24 14:19:14 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,18 @@ Route::get('/', function () {
 Route::get('/hello', function () {
     return "ma nigga";
 });
+Route::get('/profile', function () {
+    return view('profile');
+});
+Route::get('/experience', function () {
+    return view('experience');
+});
+Route::get('/portofolio', function () {
+    return view('portofolio');
+});
+Route::get('/demo', function () {
+    return view('demo');
+ });
+Route::get('user/{name?}', function ($name = null) {
+    return $name;
+});
